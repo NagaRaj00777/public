58f550cbc184fbe9a2a35fba2c1a06e3aad52877
NagaRaj00777 <2200090088@kluniversity.in>
2024-10-09 14:40:11 +0530

    first commit

---
+++ src/main/java/admin_user/JfsdProjectApplication.java
@@ -0,0 +1,14 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdProjectApplication.class, args);
+	}
+
+}
